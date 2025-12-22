ce8e77f4a99b8962418f68672096ad7062a41df8
gmorikawa <morikawa.gabriel@outlook.jp>
2025-12-22 10:53:28 +0900

    refactor: Correction on parameter name in Repository interface

--- src/shared/repository.ts
+++ src/shared/repository.ts
@@ -1,7 +1,7 @@
 export interface Repository<Id, Entity, PartialEntity> {
     findAll(): Promise<Entity[]>;
     findById(id: Id): Promise<Entity | null>;
-    create(user: PartialEntity): Promise<Entity>;
-    update(id: Id, user: Entity): Promise<Entity | null>;
+    create(entity: PartialEntity): Promise<Entity>;
+    update(id: Id, entity: Entity): Promise<Entity | null>;
     delete(id: Id): Promise<boolean>;
 }
