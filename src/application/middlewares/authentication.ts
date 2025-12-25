51bae8d7a8de2bd70b2f732bc3d5b4790c1a410f
gmorikawa <morikawa.gabriel@outlook.jp>
2025-12-25 23:44:45 +0900

    feat: Accepting auth tokens in search params

--- src/application/middlewares/authentication.ts
+++ src/application/middlewares/authentication.ts
@@ -13,20 +13,41 @@ const publicPaths = [
     "/api/auth/login"
 ];
 
+function extractTokenFromHeader(request: Request): Token | null {
+    const authHeader = request.headers["authorization"];
+
+    if (!authHeader) {
+        return null;
+    }
+
+    return authHeader as Token;
+}
+
+function extractTokenFromParams(request: Request): Token | null {
+    const tokenParam = request.query["auth_token"];
+
+    if (!tokenParam) {
+        return null;
+    }
+
+    return tokenParam as Token;
+}
+
 export async function authentication(request: Request, response: Response, next: Function) {
     if (publicPaths.includes(request.url)) {
         next();
         return;
     }
 
-    if (!request.headers["authorization"]) {
+    const token = extractTokenFromHeader(request) ?? extractTokenFromParams(request);
+
+    if (!token) {
         response.statusCode = 401;
         response.send(JSON.stringify({ error: "Unauthorized" }));
         return;
     }
 
     try {
-        const token = request.headers["authorization"] as Token;
         const secretKey = Environment.TOKEN_SECRET_KEY;
         const tokenGenerator = new JwtTokenGenerator();
         const payload = await tokenGenerator.verify<LoggedUser>(token, secretKey);
