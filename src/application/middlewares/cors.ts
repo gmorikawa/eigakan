c87d738cb69341d2784c0a220759a8c9f41a3937
gmorikawa <morikawa.gabriel@outlook.jp>
2025-12-26 11:09:22 +0900

    refactor: Cors middleware better variable naming

--- src/application/middlewares/cors.ts
+++ src/application/middlewares/cors.ts
@@ -2,13 +2,13 @@ import { Environment } from "@application/configuration/environment.js";
 import type { Request } from "@application/types/request.js";
 import type { Response } from "@application/types/response.js";
 
-export function cors(req: Request, res: Response, next: Function) {
-    res.setHeader("Access-Control-Allow-Origin", Environment.WEB_CLIENT_URL);
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+export function cors(request: Request, response: Response, next: Function) {
+    response.setHeader("Access-Control-Allow-Origin", Environment.WEB_CLIENT_URL);
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-    if (req.method === "OPTIONS") {
-        res.sendStatus(204);
+    if (request.method === "OPTIONS") {
+        response.sendStatus(204);
     } else {
         next();
     }
